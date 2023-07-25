7f7ab38753fc9decdb8b806182c254d6e66b95f0
DileepGaja <dileepkm12@gmail.com>
2023-07-25 16:24:10 +0530

    Architect Asian development

--- src/Section32_GIT/aLearning_Java_Streams.java
+++ src/Section32_GIT/aLearning_Java_Streams.java
@@ -15,90 +15,6 @@ import org.testng.annotations.Test;
 public class aLearning_Java_Streams 
 {
 	
-	//count the number of names starting with alphabet "A" in the list
-	/*@Test
-	public void regular()
-	{
-    	ArrayList<String> names= new ArrayList<String>();
-    	
-    	names.add("Dileep");
-    	names.add("Abhi");
-    	names.add("Boss");
-    	names.add("Aon");
-    	names.add("Aaxmi");
-    	
-    	int count = 0;
-    	
-    	for(int i=0; i<names.size(); i++)
-    	{
-    		 String actual=names.get(i);
-    		 if(actual.endsWith("A"))
-    		 {
-				count++;
-    		 }
-    		System.out.println(count);
-    	}
-    	
-    	
-	}
-    */
-   
-    @Test
-    public void StreamFilter()
-    {
-        ArrayList<String> names= new ArrayList<String>();
-    	
-    	names.add("Dileep");
-    	names.add("Abhi");
-    	names.add("Boss");
-    	names.add("Aon");
-    	names.add("Aaxmi");
-    	
-    	//there is no lise for intermediate operation if there is no terminal operation
-    	//terminal operation will execute only if intermediate operation (filter) returns true
-    	//we can create stream
-    	//how to use filter in stream API
-    	
-    	Long c=names.stream().filter(s->s.startsWith("B")).count();
-    	System.out.println("the count is "+c);
-    	
-    	//to make stream compatible we can create stream
-    	long d=Stream.of("Dileep","Abhi","Boss","Aon","Aaxmi").filter(s->s.startsWith("A")).count();      
-    	System.out.println("the count of d is "+d);
-    	
-    	//print the names of array list
-    	//names.stream().filter(s->s.length()>4).forEach(s->System.out.println(s));
-    	names.stream().filter(s->s.length()>4).limit(1).forEach(s->System.out.println("the length()>4 is  "+s));
-    }
-    
-    @Test
-    public void StreamMap()
-    {
-       ArrayList<String> names= new ArrayList<String>();
-    	names.add("Dileep");
-    	names.add("Abhi");
-    	names.add("Boss");
-    	names.add("Aon");
-    	names.add("Aaxmi");
-    	
-    	//print names which have last letter as "a" with uppercase
-    	Stream.of("Dileep","Abhi","Boss","Aon","Aaxmi").filter(s->s.endsWith("i")).map(s->s.toUpperCase())
-    	.forEach(s->System.out.println(s));
-    	
-    	//print names which have last letter as "a" with uppercase and sorted
-    	List <String> names1= Arrays.asList("Poi","Abhi","Boss","Aon","Aaxmi");
-    	names1.stream().filter(s->s.endsWith("i")).sorted().map(s->s.toUpperCase())
-    	.forEach(s->System.out.println(s));
-    	
-    	//merging two different list
-    	Stream<String> newStream= Stream.concat(names.stream(), names1.stream());
-    	//newStream.sorted().forEach(s->System.out.println(s));
-    	
-    	boolean flag=newStream.anyMatch(s->s.equalsIgnoreCase("Boss"));
-    	System.out.println(flag);
-    	Assert.assertTrue(flag);
-    }
-    
     @Test
     public void StreamCollect()
     {
@@ -119,6 +35,32 @@ public class aLearning_Java_Streams
     	System.out.println("the 2nd index is "+value.get(2));
     	System.out.println("the 1st index is "+value.get(1));
     	System.out.println("the 1st index is "+value.get(1));
+    }
+    
+    @Test
+    public void gut()
+    {
+    	System.out.println("dboss");
+    	System.out.println("ponnu");
+    	System.out.println("darling");
+    }
+    
+    @Test
+    public void ret()
+    {
+    	System.out.println("boss");
+    	System.out.println("naina");
+    	System.out.println("bhavana");
+    }
+    
+    @Test
+    public void StreamCol()
+    {
+    	
+    	System.out.println("TEST");
+    	System.out.println("mobile");
+    	System.out.println("android");
+
     }
     
 }
