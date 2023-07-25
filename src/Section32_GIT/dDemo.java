1bef8b19b86390a915ae1e367112a14d28a64150
DileepGaja <dileepkm12@gmail.com>
2023-07-25 17:03:03 +0530

    final commit by Asian development team

---
+++ src/Section32_GIT/dDemo.java
@@ -0,0 +1,13 @@
+package Section32_GIT;
+
+import org.testng.annotations.Test;
+
+public class dDemo {
+	
+@Test
+public void demo()
+{
+	System.out.println("mobile");
+	System.out.println("android");
+}
+}
